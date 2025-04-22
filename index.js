14c0222a4ed2d893396eedd442dcc4ae4e29074e
ProjectInfinitix <rick.andersons2000@gmail.com>
2025-04-22 16:16:28 -0400

    Delete index.js

--- index.js
+++
@@ -1,7 +0,0 @@
-import { AppRegistry } from 'react-native';
-import App from './App';
-
-AppRegistry.runApplication('main', {
-  initialProps: {},
-  rootTag: document.getElementById('app-root'),
-});
